6d180ddfaedc03783b6482abb6bb8ecfaaab330a
GSSaranya <saranya.ramalakshmii@gmail.com>
2020-08-07 15:55:45 +0530

    added comments

--- src/data/ReadingPDF.java
+++ src/data/ReadingPDF.java
@@ -1,6 +1,5 @@
 package data;
 
-
 import java.io.File;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -11,65 +10,65 @@ import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 public class ReadingPDF {
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
+		try {
+			
+			//Loading the PDF file from local path
+			
+			PDDocument document = PDDocument.load(new File("C:\\Users\\91784\\Downloads\\Documents\\RELIANCE.NS.pdf"));
+			document.getClass();
 
-try {
-PDDocument document = PDDocument.load(new File("C:\\Users\\91784\\Downloads\\Documents\\RELIANCE.NS.pdf"));
+			if (!document.isEncrypted()) {
 
-document.getClass();
+				// Reading and storing it in String 
+				
+				PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+				stripper.setSortByPosition(true);
 
-            if (!document.isEncrypted()) {
+				PDFTextStripper tStripper = new PDFTextStripper();
 
-                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
-                stripper.setSortByPosition(true);
+				String pdfFileInText = tStripper.getText(document);
 
-                PDFTextStripper tStripper = new PDFTextStripper();
+				String lines[] = pdfFileInText.split("\\r?\\n");
 
-                String pdfFileInText = tStripper.getText(document);
-               
-             
-                String lines[] = pdfFileInText.split("\\r?\\n");
-               
-                BigDecimal totVol = BigDecimal.valueOf(0);
-                BigDecimal closeVol = BigDecimal.valueOf(0);
-               
-                int days = 0;
-               
-               
-                for (int  i=1;i <lines.length;i++) {
-               
-                String[] stringArray = lines[i].split(" ");
-               
-                String close = stringArray[4];
-                String vol = stringArray[6];
-               
-                if(vol == null || vol.equals("null")) continue;
-               
-                BigDecimal convertedVol = new BigDecimal(vol);
-                totVol = totVol.add(convertedVol);
-               
-                BigDecimal convertedClose = new BigDecimal(close);
-                BigDecimal multiplied = convertedClose.multiply(convertedVol);
-               
-                closeVol = closeVol.add(multiplied);
-                days++;
-               
-                }
-               
-                BigDecimal convertedDays  = new BigDecimal(days);
-               
-                BigDecimal totalCloseVol = convertedDays.multiply(closeVol);
-               
-                BigDecimal VWMA = totalCloseVol.divide(totVol,2,RoundingMode.HALF_UP);
-               
-                System.out.println("Volume Weighted Moving Average is " +VWMA);
-            }
-}catch(Exception e) {
-e.printStackTrace();
+				BigDecimal totVol = BigDecimal.valueOf(0);
+				BigDecimal closeVol = BigDecimal.valueOf(0);
+
+				int days = 0;
+
+				for (int i = 1; i < lines.length; i++) {
+
+					String[] stringArray = lines[i].split(" ");
+
+					String close = stringArray[4];
+					String vol = stringArray[6];
+
+					if (vol == null || vol.equals("null"))
+						continue;
+
+					BigDecimal convertedVol = new BigDecimal(vol);
+					totVol = totVol.add(convertedVol);
+
+					BigDecimal convertedClose = new BigDecimal(close);
+					BigDecimal multiplied = convertedClose.multiply(convertedVol);
+
+					closeVol = closeVol.add(multiplied);
+					days++;
+
+				}
+
+				//Calculating VWMA
+				BigDecimal convertedDays = new BigDecimal(days);
+
+				BigDecimal totalCloseVol = convertedDays.multiply(closeVol);
+
+				BigDecimal VWMA = totalCloseVol.divide(totVol, 2, RoundingMode.HALF_UP);
+
+				System.out.println("Volume Weighted Moving Average is " + VWMA);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-}
-
-}
-
-
